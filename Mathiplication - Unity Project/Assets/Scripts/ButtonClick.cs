45922e9679c630552a4fbb3880667c57896205a3
Nathan Eck <xyzzynot2@yahoo.com>
2019-09-16 22:15:44 -0500

    Meatball In UI 0.1
    Meatballs are images, not VSG so they are in UI.
    Therefore, the meatballs are buttons. 
    Button script jumps to the middle (hope to make them fall on the ground and roll).

--- Mathiplication - Unity Project/Assets/Scripts/ButtonClick.cs
+++ Mathiplication - Unity Project/Assets/Scripts/ButtonClick.cs
@@ -8,13 +8,14 @@ public class ButtonClick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 1);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     TMP_Text buttonTextMesh;
@@ -23,5 +24,10 @@ public class ButtonClick : MonoBehaviour
     {
         buttonTextMesh = this.GetComponentInChildren<TMP_Text>();
         buttonTextMesh.text = "Clicked";
+
+        float objectWrapper = buttonTextMesh.GetComponent<RectTransform>().rect.height / 2;
     }
+
+    
+
 }
