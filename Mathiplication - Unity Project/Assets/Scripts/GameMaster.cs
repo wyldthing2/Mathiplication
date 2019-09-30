8386976c0f4072a8f6bf3d4e0f48c0e842e2aa03
Nathan Eck <xyzzynot2@yahoo.com>
2019-09-30 10:50:00 -0500

    Progress Bar + Android Platform + Unity Update2019.2.6f1
    -Added BoxManTruck png
    -Switched to Android
    -Progress bar that increases and decreases with player answers.
    -Updated Unity

--- Mathiplication - Unity Project/Assets/Scripts/GameMaster.cs
+++ Mathiplication - Unity Project/Assets/Scripts/GameMaster.cs
@@ -12,6 +12,11 @@ public class GameMaster : MonoBehaviour
     [SerializeField] int num1;
     [SerializeField] int num2;
 
+    [SerializeField] public int MeatballCount = 0;
+    [SerializeField] public int CurrentGoal = 100;
+    [SerializeField] public float CurrentGoalProgress = 0;
+    [SerializeField] ProgressTracker myProgressTracker;
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,15 +40,34 @@ public class GameMaster : MonoBehaviour
         Debug.Log(answer);
         if (answer == num1 * num2)
         {
+            if (CurrentGoalProgress < 1)
+            {
+                MeatballCount += answer;
+                CurrentGoalProgress = (float)MeatballCount / (float)CurrentGoal;
+                if(CurrentGoalProgress > 1 )
+                {
+                    CurrentGoalProgress = 1;
+                }
+                myProgressTracker.SetProgressBar(CurrentGoalProgress);
+            }
             
             //activate CORRECT
+            Debug.Log("Progress " + CurrentGoalProgress);
             setNumbers();
         }
         else
         {
             //activate incorrect and make it fade out and shrink and lose points!
             Debug.Log("Wrong, " + num1*num2);
-            
+            MeatballCount -= Mathf.RoundToInt((float)MeatballCount * (float).5);
+            if (MeatballCount < 0)
+            {
+                MeatballCount = 0;
+            }
+            CurrentGoalProgress = (float)MeatballCount / (float)CurrentGoal;
+            myProgressTracker.SetProgressBar(CurrentGoalProgress);
+
+
         }
     }
 
